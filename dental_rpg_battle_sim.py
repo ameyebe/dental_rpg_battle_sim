ecb9cd0693136b11e6eab7f92877420dfce8ec2a
ameyebe <52386734+ameyebe@users.noreply.github.com>
2019-07-01 10:21:42 +1200

    Add files via upload

---
+++ dental_rpg_battle_sim.py
@@ -0,0 +1,8 @@
+# dental_rpg_battle_sim.py
+# date: 1/07/2019
+# Author: Benjamin Ameye
+
+def dental_game():
+    print("Welcome to my Dental RPG battle sim, you'll fight bacteria and infected teeth with a team of characters and attack in turns")
+
+dental_game()
